70096aafffe629cc09bdeb944dd38c02cd8afb75
boomerboss <73359759+boomerboss@users.noreply.github.com>
2023-12-02 20:27:58 -0500

    Updated Day 1 part 2 attempt, failed problem

--- day-one/One.rb
+++ day-one/One.rb
@@ -13,21 +13,47 @@ def get_first_last_integers(str)
 end
 
 def convert_written_digits_to_numerals(str)
-    str.sub!("one", "1")
-    str.sub!("two", "2")
-    str.sub!("three", "3")
-    str.sub!("four", "4")
-    str.sub!("five", "5")
-    str.sub!("six", "6")
-    str.sub!("seven", "7")
-    str.sub!("eight", "8")
-    str.sub!("nine", "9")
-    str
+    digit_words_to_numbers = {
+      'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4',
+      'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9'
+    }
+  
+    # Sort the digit words by length so that the longest words are tried first
+    sorted_digit_words = digit_words_to_numbers.keys.sort_by(&:length).reverse
+  
+    new_str = ''
+    i = 0
+  
+    while i < str.length
+      matched = false
+  
+      # Try to match the longest word possible at the current position
+      sorted_digit_words.each do |word|
+        if str[i, word.length].casecmp?(word)
+          # If a match is found, append the corresponding digit to the new string
+          new_str << digit_words_to_numbers[word]
+          # Skip ahead by the length of the matched word
+          i += word.length
+          matched = true
+          break
+        end
+      end
+  
+      # If no match was found, just append the current character
+      unless matched
+        new_str << str[i]
+        i += 1
+      end
+    end
+  
+    new_str
 end
+   # Couldn't get this to work. I don't know how to deal with overlap
 
-line = "xtwone3four"
+line = "oneight"
 formatted_line = convert_written_digits_to_numerals(line)
-puts get_first_last_integers(formatted_line) # doesn't work because it needs to be in order, so two is formatted before one
+puts formatted_line
+# puts get_first_last_integers(formatted_line) # doesn't work because it needs to be in order, so two is formatted before one
 
 
 
